a14c0e5e2e7274fac2b61890bcf78688c4b125c3
mrvldo <mariorivaldo86@gmail.com>
2022-10-03 10:42:49 +0700

    second commit

--- pindahLayout/app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ pindahLayout/app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -2,13 +2,39 @@ package com.if3b.pindahlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Deklarasi
+    Button btnPindah;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final RelativeLayout relativeLayout;
+
+//      Cari Id
+        btnPindah = findViewById(R.id.btn_pindah);
+        relativeLayout = findViewById(R.id.tv_selamat_datang);
+
+        btnPindah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundResource(R.color.cool);
+                Intent pindah = new Intent(MainActivity.this, secondActivity.class);
+                startActivity(pindah);
+            }
+        });
+
+
     }
 }
